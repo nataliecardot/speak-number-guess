358682165b11ece674fd9762bef87884fd9e3ecf
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-26 15:38:23 -0800

    Check guessed number and provide hint

--- script.js
+++ script.js
@@ -16,8 +16,45 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  // writeMessage(msg);
-  // checkNumber(msg);
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Output what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div><p>You said:</p></div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = "<div><p>That's not a valid number</p></div>";
+    return;
+  }
+
+  // Check number is in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div><p>Number must be between 1 and 100</p></div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h1 class="h2">Congrats! You've guessed the correct number!</h1><br><br><p>The number was: ${randomNum}</p>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div><p>GO LOWER</p></div>';
+  } else {
+    msgEl.innerHTML += '<div><p>GO HIGHER</p></div>';
+  }
 }
 
 // Generate random number
